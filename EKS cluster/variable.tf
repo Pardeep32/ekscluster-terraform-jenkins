670f53da3805770092f0663e566a1220df2c5c63
pardeep kaur <137564227+Pardeep32@users.noreply.github.com>
2024-02-29 14:49:38 -0500

    Create variable.tf

---
+++ EKS cluster/variable.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "ca-central-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC Cidr"
+  type        = string
+  default     = "192.68.0.0/16"
+}
+
+
+variable "private_subnets" {
+  description = "private subnet"
+  type        = list(string)
+  default     = ["198.68.1.0/24", "198.68.2.0/24", "198.68.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "public subnet cidr"
+  type        = list(string)
+  default     = ["192.68.4.0/24", "198.68.5.0/24", "198.68.4.0/24"]
+}
+
+
+variable "instance_type" {
+  default = "t2.micro"
+}
