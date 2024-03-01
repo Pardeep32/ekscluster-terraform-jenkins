4ccb3c34521e051374b3f58f8ba6d7cfbe16ed8a
pardeep kaur <137564227+Pardeep32@users.noreply.github.com>
2024-03-01 15:31:06 -0500

    Update main.tf

--- EKS cluster/main.tf
+++ EKS cluster/main.tf
@@ -30,70 +30,26 @@ module "vpc" {
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  source = "terraform-aws-modules/eks/aws"
 
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.24"
-   
-  cluster_endpoint_public_access = true
-  
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
 
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-  // control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
-
-  # EKS Managed Node Group(s)
-  /*  eks_managed_node_group_defaults = {
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-  }
-  */
 
   eks_managed_node_groups = {
-    example = {
+    nodes = {
       min_size     = 1
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t2.micro"]
-      // capacity_type  = "SPOT"
+      instance_type = ["t2.small"]
     }
   }
 
-  # Cluster access entry
-  # To add the current caller identity as an administrator
-    enable_cluster_creator_admin_permissions = true
-
-  access_entries = {
-    # One access entry with a policy associated
-    example = {
-      kubernetes_groups = []
-      principal_arn     = "arn:aws:iam::123456789012:role/something"
-
-      policy_associations = {
-        example = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-          access_scope = {
-            namespaces = ["default"]
-            type       = "namespace"
-          }
-        }
-      }
-    }
-  }
-  
   tags = {
     Environment = "dev"
     Terraform   = "true"
