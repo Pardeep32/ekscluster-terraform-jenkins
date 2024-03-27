898ac3904f4f05f0198441130d7cc10393514bb8
pardeep kaur <137564227+Pardeep32@users.noreply.github.com>
2024-03-27 16:17:57 -0400

    Update provider.tf

--- EKS cluster/provider.tf
+++ EKS cluster/provider.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.38.0"
+      source = "hashicorp/aws"
+      version = "5.42.0"
     }
   }
 }
+
 
 provider "aws" {
   region = var.region
